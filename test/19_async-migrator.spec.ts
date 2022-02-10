0d2560723b965ac300173c45a8594a2065e16f6c
Harald Fischer <harald@balena.io>
2022-02-10 23:57:58 +0100

    skip async migration test
    Signed-off-by: Harald Fischer <harald@balena.io>

--- test/19_async-migrator.spec.ts
+++ test/19_async-migrator.spec.ts
@@ -1,30 +1,36 @@
 import { initSupertest } from './test-lib/supertest';
 import * as _ from 'lodash';
 import { sbvrUtils, dbModule } from '@balena/pinejs';
+import { expect } from 'chai';
+import * as fs from 'fs';
 
 describe('Async Migrations', async function () {
 	this.timeout(60000);
 
-	before(async function () {
-		const testConfig = require('../config');
+	describe('data catch up', function () {
+		beforeEach(async function () {
+			const testConfig = require('../config');
 
-		testConfig.models[0].migrationsPath =
-			__dirname + '/fixtures/19-async-migrator/01-migrations/';
+			testConfig.models[0].migrationsPath =
+				__dirname + '/fixtures/19-async-migrator/01-migrations/';
 
-		// delete cached migrations
-		testConfig.models[0].migrations = {};
-		await initSupertest({ initConfig: testConfig, deleteDatabase: true });
-	});
+			// delete cached migrations
+			testConfig.models[0].migrations = {};
+			await initSupertest({ initConfig: testConfig, deleteDatabase: true });
+		});
 
-	it('wait for first catchup', function (done) {
-		setInterval(async () => {
-			try {
+		it('wait for first catch up', function (done) {
+			const test = setInterval(async () => {
 				let result;
 				await sbvrUtils.db.transaction(async (tx) => {
-					result = await tx.executeSql(
-						`
-SELECT * FROM test;`,
-					);
+					try {
+						result = await tx.executeSql(
+							`
+						SELECT * FROM test;`,
+						);
+					} catch (err) {
+						// just ignore it - case will fail after timeout
+					}
 				});
 				const unequalRows = (result as unknown as dbModule.Result).rows.filter(
 					(row) => {
@@ -33,11 +39,63 @@ SELECT * FROM test;`,
 				);
 
 				if (unequalRows.length === 0) {
+					clearInterval(test);
 					done();
 				}
-			} catch (err) {
-				// just ignore it - case will fail after timeout
-			}
-		}, 1000);
+			}, 1000);
+		});
+	});
+
+	describe.only('async migration skip', function () {
+		beforeEach(async function () {
+			const testConfig = require('../config');
+
+			testConfig.models[0].migrationsPath =
+				__dirname + '/fixtures/19-async-migrator/02-skip-async/';
+
+			// delete cached migrations
+			testConfig.models[0].migrations = {};
+			await initSupertest({ initConfig: testConfig, deleteDatabase: true });
+
+			// manually calling the init data creation sql query as the model gets
+			// executed for the first time and sync migrations are skipped
+			const initDataSql = await fs.promises.readFile(
+				__dirname +
+					'/fixtures/19-async-migrator/02-skip-async/0001-init-data.sync.sql',
+				'utf8',
+			);
+
+			await sbvrUtils.db.transaction(async (tx) => {
+				try {
+					await tx.executeSql(initDataSql);
+				} catch (err) {
+					// just ignore it - case will fail after timeout
+				}
+			});
+		});
+
+		it.only('no async row migration should take place', function (done) {
+			setTimeout(async () => {
+				let result: dbModule.Result = {} as dbModule.Result;
+
+				await sbvrUtils.db.transaction(async (tx) => {
+					try {
+						result = await tx.executeSql(
+							`
+						SELECT * FROM test
+						WHERE  "columnA" = "columnC";`,
+						);
+					} catch (err) {
+						// just ignore it - case will fail after timeout
+					}
+				});
+				try {
+					expect(result?.rows).to.be.empty;
+					done();
+				} catch (err) {
+					done(err);
+				}
+			}, 2000);
+		});
 	});
 });
